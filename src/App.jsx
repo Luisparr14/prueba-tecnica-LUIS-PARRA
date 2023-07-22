b9222062ea33c5efc30d1bc9dc10f9e871e5878e
Luis Angel <luisparr14@gmail.com>
2023-07-22 09:14:50 -0500

    test: reset button

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import UserList from './components/UserList'
+import { COLUMN_NAMES } from './constants'
+import useApi from './hooks/useApi'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+  const { useGet } = useApi()
+  const [refetch, setRefetch] = useState(false)
+
+  const { data: users, loading, setData: setUsers } = useGet('https://randomuser.me/api/?results=100', refetch)
+
+  const handleDelete = (email) => {
+    const newUsers = users?.results.filter((user) => user.email !== email)
+    setUsers({ ...users, results: newUsers })
+  }
+
+  const handleReset = () => {
+    setRefetch(!refetch)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='text-6xl font-bold my-6 text-center'>Lista de usuarios</h1>
+      <button onClick={handleReset} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+        Reset
+      </button>
+      <table className='table w-full'>
+        <thead>
+          <tr>
+            {COLUMN_NAMES.map((columnName, index) => (
+              <th key={index} className='px-4 py-2'>
+                {columnName}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <UserList users={users?.results} loading={loading} onDelete={handleDelete} />
+        </tbody>
+      </table>
     </>
   )
 }
