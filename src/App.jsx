d6ad4dabf769f1b495d185ca2d19b0b0c24e6c8d
Luis Angel <luisparr14@gmail.com>
2023-07-22 11:59:56 -0500

    implement sortBy country and add Action component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import useApi from './hooks/useApi'
 import Table from './ui/Table'
+import Actions from './ui/Actions'
 import { COLUMN_NAMES } from './constants'
 import './App.css'
 
 function App () {
   const { useGet } = useApi()
   const [refetch, setRefetch] = useState(false)
+  const [sortBy, setSortBy] = useState(null)
+  const [colorizeRows, setColorizeRows] = useState(false)
 
   const {
     data: users,
@@ -23,20 +26,48 @@ function App () {
     setRefetch(!refetch)
   }
 
+  const handleColorizeRows = () => {
+    setColorizeRows(!colorizeRows)
+  }
+
+  const usersOrderByCountry = useMemo(() => {
+    const userList = JSON.parse(JSON.stringify(users))
+
+    return userList?.results?.sort((a, b) => {
+      if (a.location.country < b.location.country) {
+        return -1
+      }
+      if (a.location.country > b.location.country) {
+        return 1
+      }
+      return 0
+    })
+  }, [users])
+
+  const handleSortByCountry = () => {
+    if (sortBy === 'country') setSortBy(null)
+    else setSortBy('country')
+  }
+
+  const userData = useMemo(() => {
+    switch (sortBy) {
+      case 'country':
+        return usersOrderByCountry
+      default:
+        return users?.results
+    }
+  }, [sortBy, users, usersOrderByCountry])
+
   return (
     <>
       <h1 className='text-6xl font-bold my-6 text-center'>Lista de usuarios</h1>
-      <button
-        onClick={handleReset}
-        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-      >
-        Reset
-      </button>
+      <Actions onColorizeRows={handleColorizeRows} onReset={handleReset} onSort={handleSortByCountry} />
       <Table
         columns={COLUMN_NAMES}
-        data={users?.results}
+        data={userData}
         loading={loading}
         onDelete={handleDelete}
+        colorizeRows={colorizeRows}
       />
     </>
   )
